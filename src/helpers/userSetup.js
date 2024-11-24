e5b8d94814230081dc61bd53ca6b0b3f64d344f4
tim@mbp <timforrer16@gmail.com>
2024-11-25 00:20:28 +0900

    add ruby markdown parser

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,6 +1,7 @@
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+  md.use(require("markdown-it-ruby"))
 }
 
 function transformImages(content) {
